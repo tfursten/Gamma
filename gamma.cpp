0964b616d9088285438f13e333ad16c44e721996
tfursten <tfursten@asu.edu>
2015-01-30 14:57:32 -0700

    clean up

--- gamma.cpp
+++ gamma.cpp
@@ -7,44 +7,8 @@
 
 using namespace std;
 
-inline int wrap_around(int x, int w) {
-    return ((x%w)+w)%w;
-}
-
-inline int xy2i(int x, int y, int mx, int my) {
-    assert(0 <= x && x < mx);
-    assert(0 <= y && y < my);
-    return x*my+y;
-}
-
-int absorbing(int x, int y, int maxX, int maxY)
-{
-    if (x >= 0 && x < maxX && y >= 0 && y < maxY)
-        return xy2i(x,y,maxX,maxY);
-    return -1;
-}
-
-int periodic(int x, int y, int maxX, int maxY)
-{
-    int newX = wrap_around(x,maxX);
-    int newY = wrap_around(y,maxY);
-    return xy2i(newX,newY,maxX,maxY);
-}
-
-int disperse_cont(xorshift64& rand, int x, int y, double d, int mx, int my)
-{
-    double a = rand.get_double52() * 2.0 * M_PI;
-    double newX = floor(d*cos(a)+x+0.5);
-    double newY = floor(d*sin(a)+y+0.5);
-    int i = periodic(newX,newY,mx,my);
-    return i;
-
-}
-
-
-double gamma_bigA(xorshift64& rand, double a, double b, double dd, double cc)
-{
-    //Marsalgia2000
+double gamma(xorshift64& rand, double a, double b, double dd, double cc){
+//Marsalgia2000
     double d;
     for(;;){
         double x = rand_normal(rand,0.0,1);
@@ -61,11 +25,43 @@ double gamma_bigA(xorshift64& rand, double a, double b, double dd, double cc)
             break;
         }
     }
-    //cout << d*b << endl;
     return d/b;
 }
 
-double gamma_smallA(xorshift64& rand, double a, double b, double L, double w, double r){
+
+
+
+int main(){
+    xorshift64 myrand;
+    myrand.seed(46546546001);
+    double scale = 1/3.0; //theta
+    double sigma = 4;
+    double alpha = (sigma*sigma*scale);
+    cout << "Alpha: " << alpha << endl;
+    assert(alpha>=1);
+    double sum = 0.0;
+    int it = 10000000;
+    vector<double> var(it);
+    double dd = alpha-(1/3.0);
+    double cc = 1/sqrt(9*dd);
+    for(int i = 0; i<it; i++){
+        double d = gamma(myrand, alpha, scale, dd, cc);
+        sum += d;
+        var[i] = d;
+    }
+
+    double variance;
+    double mean = sum/double(it);
+    for(int i = 0; i<it; i++){
+        variance += (var[i]-mean)*2;
+    }
+    cout << "Mean: " << mean << endl;
+    cout << "Variance: " << variance << endl;
+    return 0;
+}
+
+/*double gamma_smallA(xorshift64& rand, double a, double b, double L, double w, double r){
+    //Martin2013
     double d;
     for(;;){
         double u = rand.get_double52();
@@ -91,50 +87,4 @@ double gamma_smallA(xorshift64& rand, double a, double b, double L, double w, do
     d = exp(d);
     //cout << d << endl;
     return d;
-}
-
-
-int main(){
-    xorshift64 myrand;
-    double sigma = .5;
-    double scale = 1/0.5;
-    //double alpha = 1;
-    double alpha = scale * sigma * sigma;
-    //double alpha = (sigma*sigma)/(scale);
-    cout << "Alpha: " << alpha << endl;
-    assert(alpha>0);
-    double sum = 0.0;
-    int it = 10000000;
-    vector<double> var(it);
-    //if(alpha>=1){
-    double dd = alpha-(1/3.0);
-    double cc = 1/sqrt(9*dd);
-    //cout << "BIG ALPHA" << endl;
-    for(int i = 0; i<it; i++){
-        double d = gamma_bigA(myrand, alpha, scale, dd, cc);
-        sum += d;
-        var[i] = d;
-    }
-    //}
-    /*
-    else{
-        double L = 1/alpha;
-        double w = alpha/exp(1-alpha);
-        double r = 1/(1+w);
-        cout << "SMALL ALPHA" << endl;
-        for(int i = 0; i<it; i++){
-            double d = gamma_smallA(myrand, alpha, scale, L, w, r);
-            sum += d;
-            var[i] = d;
-        }
-    }
-    */
-    double variance;
-    double mean = sum/double(it);
-    for(int i = 0; i<it; i++){
-        variance += (var[i]-mean)*2;
-    }
-    cout << "Mean: " << mean << endl;
-    cout << "Variance: " << variance << endl;
-    return 0;
-}
+}*/
